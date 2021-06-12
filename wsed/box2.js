2a82d2b5e11fdb184764dbc110d83c2fd3df4b2e
kusumasharath3 <75429712+kusumasharath3@users.noreply.github.com>
2021-06-12 10:23:55 +0530

    Add files via upload

---
+++ wsed/box2.js
@@ -0,0 +1,24 @@
+class BoxA{
+    constructor(x, y, width, height) {
+      var options = {
+        restitution: 0.4,
+        friction: 0.9,
+      };
+        
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        push();
+        //translate(this.body.position.x, this.body.position.y);
+        var pos =this.body.position;
+        rectMode(CENTER);
+        fill("yellow");
+        rect(pos.x, pos.y, this.width, this.height);
+        
+        pop();
+      }
+  }
+  
